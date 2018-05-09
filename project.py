39e1271f6546c5fa2985500833ab1dfb0f283d94
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-05-09 19:41:04 +0300

    added delete post

--- project.py
+++ project.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-# pgAdmin 3
+# если используешь pgAdmin 3
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/nameDB'
-# Heroku
+# если используешь Heroku
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hxeqjnnqpawpvd:da95269ab4f16bd45f511569568d791c6d491671ddb216c809701632eced90cb@ec2-54-247-89-189.eu-west-1.compute.amazonaws.com:5432/dc4dqg53di349k'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -15,9 +15,11 @@ class Comments(db.Model):
 	name = db.Column(db.String(20))
 	comment = db.Column(db.String(200))
 
+# Главная страница, в result загружается база данных
 @app.route('/')
 def index():
 	result = Comments.query.all()
+	result.reverse() # Обратный порядок
 	return render_template('index.html', result=result)
 
 @app.route('/sign')
@@ -34,6 +36,21 @@ def process():
 	db.session.commit()
 
 	return redirect(url_for('index'))
+
+# Включать для удаления постов (Удаляю по id записи)
+@app.route('/delete/<string:id>')
+def delete(id):
+	result = Comments.query.filter_by(id=id).first()
+	db.session.delete(result)
+	db.session.commit()
+
+	return redirect(url_for('index'))
+
+# Дополнительно
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
